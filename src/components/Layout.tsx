16856eeb63986e183a26bbb42b04cb278d8fd5f3
Juan Duran <pichocho@gmail.com>
2025-11-07 01:17:59 -0300

    Update Layout.tsx
    tentativa resolver scroll 1

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -93,7 +93,7 @@ export function Layout({ children, currentPage, onNavigate }: LayoutProps) {
         )}
       </header>
 
-      <main className="flex-1 overflow-auto">
+      <main className="flex-1 min-h-0 overflow-auto">
         {children}
       </main>
 
