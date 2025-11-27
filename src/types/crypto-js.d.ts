2507d7a89f611f94dad9a012b665a879563fcabe
juan-duran <juan-duran@users.noreply.github.com>
2025-11-27 00:30:52 -0300

    Add minimal crypto-js types

--- src/types/crypto-js.d.ts
+++ src/types/crypto-js.d.ts
@@ -1,1 +1,19 @@
-declare module 'crypto-js';
+declare module 'crypto-js' {
+  namespace CryptoJS {
+    namespace lib {
+      type WordArray = any;
+    }
+    namespace enc {
+      const Base64: {
+        stringify(wordArray: any): string;
+        parse(base64: string): any;
+      };
+      const Utf8: {
+        stringify(wordArray: any): string;
+      };
+    }
+    function HmacSHA256(message: string, secret: string): lib.WordArray;
+  }
+  const CryptoJS: typeof CryptoJS;
+  export default CryptoJS;
+}
